494b8e4e6b957074a7ab2022d4128a072ef0a06b
César León Schaus <cschaus@agropago.com>
2022-06-24 22:32:39 -0300

    [add] my first component

---
+++ src/HelloWorldApp.jsx
@@ -0,0 +1,24 @@
+//TODO: primera opcion
+/* function HelloWorldApp() {
+    return (
+        <h1>Hola mundo!!!</h1>
+    )
+}
+
+export default HelloWorldApp;
+ */
+
+//TODO: segunda opcion, 
+// tener en cuenta q no hace falta la importacion 
+// de react
+
+// import React from 'react' 
+
+export const HelloWorldApp = () => {
+  return (
+    <h1>Hello World App</h1>
+  )
+}
+
+//TODO: tercera opcion
+// export const HelloWorldApp = () => <h1>Hello World App</h1>
