ae080ebb8f78072b06fafdc6a740efec5b68a43c
César León Schaus <cschaus@agropago.com>
2022-06-24 22:34:46 -0300

    [add] use props

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,25 +1,8 @@
-// Si este valor no cambia, se recomienda que esté afuera
-// del componente
-// const newMessage = [1,2,3,4,5,6,7,8,9];
-const newMessage = {
-  message: 'Hola',
-  name: 'César'
-};
-
-// Las funciones que no son parte del componente es mejor
-// que vayan afuera, salvo en casos puntuales q si pueden
-// estar dentro del componente
-const getGreeting = (messageObj) => {
-  return `${messageObj.message} ${messageObj.name}`;
-}
-
 const FirstApp = ({ title, subTitle }) => {
   return (
     <>
-      <h1>{getGreeting(newMessage)}</h1>
-      <h2>{title}</h2>
-      {/* <code>{JSON.stringify(newMessage)}</code> */}
-      <p>Soy un subtitulo {subTitle + 1}</p>
+      <h1>{title}</h1>
+      <p>{subTitle + 1}</p>
     </>
   )
 }
