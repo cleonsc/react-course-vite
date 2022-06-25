9dca4082f82700262766f560daffe76b2a859ec7
César León Schaus <cschaus@agropago.com>
2022-06-24 23:27:59 -0300

    [add] events - click event

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,10 +1,15 @@
 import PropTypes from 'prop-types';
 
+const handleAdd = (event) => {
+  console.log(event)
+}
+
 const CounterApp = ({ value }) => {
   return (
     <>
       <h1>CounterApp</h1>
-      <h2>{value}</h2>
+      <h2> {value} </h2>
+      <button onClick={ handleAdd }> +1 </button>
     </>
   )
 }
