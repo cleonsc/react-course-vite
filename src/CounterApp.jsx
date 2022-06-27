e7ba04ccdec736c79d60185ca7fa546f0f6fa67e
César León Schaus <cschaus@agropago.com>
2022-06-27 15:26:28 -0300

    [add] useState hook

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,15 +1,20 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const handleAdd = (event) => {
-  console.log(event)
-}
 
 const CounterApp = ({ value }) => {
+  const [counter, setCounter] = useState(value);
+
+  const handleAdd = (event) => {
+    setCounter(counter + 1);
+    // setCounter((c) => c + 1); //Esta es otra opcion de setear el counter
+  }
+
   return (
     <>
       <h1>CounterApp</h1>
-      <h2> {value} </h2>
-      <button onClick={ handleAdd }> +1 </button>
+      <h2> {counter} </h2>
+      <button onClick={handleAdd}> +1 </button>
     </>
   )
 }
