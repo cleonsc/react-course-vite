1500f6230723341e00a3ddb52ff5f06db557b9c7
César León Schaus <cschaus@agropago.com>
2022-06-27 22:10:17 -0300

    [edit] demo component test

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -1,5 +1,13 @@
-test('Esta prueba no debe de fallar', () => {
-  if (0 === 1) {
-    throw new Error('No puede dividir entre cero');
-  }
+describe('Pruebas en <DemoComponent />', () => {
+  test('Esta prueba no debe de fallar', () => {
+    //1. Inicialización
+    const message1 = 'Hola Mundo';
+
+    //2. estímulo
+    const message2 = message1.trim();
+
+    //3. observar el comportamiento esperado
+    expect(message1).toBe(message2);
+  })
+
 })
