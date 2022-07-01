b26203e11720409938a820bd7718ee3231c3d14a
César León Schaus <cschaus@agropago.com>
2022-07-01 15:07:07 -0300

    [add] async-await file catch tests

--- src/base-pruebas/11-async-await.js
+++ src/base-pruebas/11-async-await.js
@@ -2,14 +2,15 @@ import fetch from 'cross-fetch';
 
 export const getImage = async () => {
   try {
-    const apiKey = 'GqSTWoH4rFJ6WACU9zsuGEn2wZSRfhXG';
+    // const apiKey = 'GqSTWoH4rFJ6WACU9zsuGEn2wZSRfhXG';
+    const apiKey = 'GqSTWoH4rFJ6WACU9zsuGEn2wZSRfhXH';
     const resp = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
     const { data } = await resp.json();
     const { url } = data.images.original;
     
     return url;
   } catch (error) {
-    console.error(error)
+    // console.error(error)
     return 'No se encontró la imagen'
   }
 }
--- tests/base-pruebas/11-async-await.test.js
+++ tests/base-pruebas/11-async-await.test.js
@@ -3,6 +3,7 @@ import { getImage } from '../../src/base-pruebas/11-async-await';
 describe('Pruebas 11-async-await', () => {
   test('getImage should return an URL image', async () => {
     const url = await getImage();
-    expect(typeof url).toBe('string');
+    // expect(typeof url).toBe('string');
+    expect(url).toBe('No se encontró la imagen');
   })
 })
