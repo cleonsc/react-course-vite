010f3bb2387d5a144d0bb7a84d2f0b5b7038d0dc
César León Schaus <cschaus@agropago.com>
2022-07-16 11:29:19 -0300

    [add] tests with simulation event click using fireEvent of testing-library

--- tests/CounterApp.test.jsx
+++ tests/CounterApp.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import CounterApp from '../src/CounterApp';
 
 
@@ -14,6 +14,45 @@ describe('Pruebas <CounterApp />', () => {
   test('should show the initial value equal to 100, <CounterApp value={initialValue} />', () => {
     render(<CounterApp value={100} />);
     expect(screen.getByText(100)).toBeTruthy();
-    expect(screen.getByRole('heading', {level:2}).innerHTML).toContain('100');
+    expect(screen.getByRole('heading', { level: 2 }).innerHTML).toContain('100');
   });
+
+  /*
+   *SIMULACIÓN DE EVENTOS 
+   */
+  describe('Events simutation in CounterApp component', () => {
+    test('it should increment with the button "+1"', () => {
+      // inicializo mi sujeto de pruebas
+      render(<CounterApp value={initialValue} />);
+
+      // disparar un evento, para ello usamos fireEvent
+      fireEvent.click(screen.getByText('+1'))
+      expect(screen.getByText('11')).toBeTruthy();
+    })
+
+    test('it should decremetn with the button "--1"', () => {
+      // inicializo mi sujeto de pruebas
+      render(<CounterApp value={initialValue} />);
+
+      // disparar un evento, para ello usamos fireEvent
+      fireEvent.click(screen.getByText('-1'))
+
+      //permite ver el estado del DOM
+      // screen.debug();
+
+      expect(screen.getByText('9')).toBeTruthy();
+    })
+
+    test('it should work the button RESET', () => {
+      render(<CounterApp value={355}/>)
+      fireEvent.click(screen.getByText('+1'));
+      fireEvent.click(screen.getByText('+1'));
+      fireEvent.click(screen.getByText('+1'));
+      // screen.debug();
+      // fireEvent.click(screen.getByText('Reset'));
+      fireEvent.click(screen.getByRole('button', {name:'btn-reset'}))
+      expect(screen.getByText(355)).toBeTruthy();
+
+    })
+  })
 })
