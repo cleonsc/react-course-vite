e18cf55910e7399935081e8078090935dabb5b8a
César León Schaus <cschaus@agropago.com>
2022-07-16 11:28:15 -0300

    [add] aria-label to identify a unique button

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -25,8 +25,8 @@ const CounterApp = ({ value }) => {
       <h1>CounterApp</h1>
       <h2> {counter} </h2>
       <button onClick={handleAdd}> +1 </button>
-      <button onClick={handleSubtract}> --1 </button>
-      <button onClick={handleReset}> Reset </button>
+      <button onClick={handleSubtract}> -1 </button>
+      <button aria-label="btn-reset" onClick={handleReset}> Reset </button>
     </>
   )
 }
